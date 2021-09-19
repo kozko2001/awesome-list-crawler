3923e34e68983bc6fd0c78b6771d7a2e97bd4d46
Jordi Coscolla <kozko2001@gmail.com>
2021-09-19 20:11:12 +0200

    add permissions to github action user to push image

--- infrastructure/infrastructure/infrastructure_stack.py
+++ infrastructure/infrastructure/infrastructure_stack.py
@@ -12,3 +12,28 @@ class InfrastructureStack(cdk.Stack):
         ecr.AuthorizationToken.grant_read(user)
 
         repository.add_lifecycle_rule(max_image_count=5)
+
+        repository.add_to_resource_policy(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=[
+                    "ecr:GetAuthorizationToken",
+                    "ecr:BatchCheckLayerAvailability",
+                    "ecr:GetDownloadUrlForLayer",
+                    "ecr:GetRepositoryPolicy",
+                    "ecr:DescribeRepositories",
+                    "ecr:ListImages",
+                    "ecr:DescribeImages",
+                    "ecr:BatchGetImage",
+                    "ecr:GetLifecyclePolicy",
+                    "ecr:GetLifecyclePolicyPreview",
+                    "ecr:ListTagsForResource",
+                    "ecr:DescribeImageScanFindings",
+                    "ecr:InitiateLayerUpload",
+                    "ecr:UploadLayerPart",
+                    "ecr:CompleteLayerUpload",
+                    "ecr:PutImage",
+                ],
+                principals=[user],
+            )
+        )
