3876efde71120adb0b51b469004d454b50261db9
Jordi Coscolla <kozko2001@gmail.com>
2025-08-13 00:41:41 +0200

    add loading

--- web/src/components/LoadingComponent.tsx
+++ web/src/components/LoadingComponent.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function LoadingComponent() {
-  return <div> Loading data...</div>;
+  const [frame, setFrame] = useState(0);
+  
+  const frames = [
+    "[ * * * ]",
+    "[ > * * ]", 
+    "[ * > * ]",
+    "[ * * > ]",
+    "[ * * < ]",
+    "[ * < * ]",
+    "[ < * * ]"
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFrame(prev => (prev + 1) % frames.length);
+    }, 200);
+    
+    return () => clearInterval(interval);
+  }, [frames.length]);
+
+  return (
+    <div style={{ fontFamily: 'monospace', textAlign: 'center', padding: '20px' }}>
+      <div>Loading awesome data...</div>
+      <div style={{ marginTop: '10px', fontSize: '14px' }}>
+        {frames[frame]}
+      </div>
+    </div>
+  );
 }
 
 export default LoadingComponent;
